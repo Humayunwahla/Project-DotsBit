1573d27a62636d894a9521c89b4b9abb69484e01
sssc <humayunwahla55@gmail.com>
2024-04-05 14:21:56 +0500

    more errors removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-"use client";
+
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { Poppins } from "next/font/google";
